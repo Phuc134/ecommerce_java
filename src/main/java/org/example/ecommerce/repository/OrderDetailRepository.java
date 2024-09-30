e895f99f8415ef7316662cb8a1bc5cf4dc7b479c
phuc <phuc@vnpt.vn>
2024-09-30 14:37:53 +0700

    .

--- src/main/java/org/example/ecommerce/repository/OrderDetailRepository.java
+++ src/main/java/org/example/ecommerce/repository/OrderDetailRepository.java
@@ -2,9 +2,10 @@ package org.example.ecommerce.repository;
 
 import org.example.ecommerce.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, String> {
     List<OrderDetail> findByOrder_Id(String orderId);
 }
--- src/main/java/org/example/ecommerce/repository/OrderRepository.java
+++ src/main/java/org/example/ecommerce/repository/OrderRepository.java
@@ -3,9 +3,10 @@ package org.example.ecommerce.repository;
 import org.aspectj.weaver.ast.Or;
 import org.example.ecommerce.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
     List<Order> findByUser_IdAndActiveTrue(String userId);
 }
